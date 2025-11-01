6b29e1983537ed257f455cccf886f6ef3236ca10
juliolouzz <jlouz.dev@gmail.com>
2025-11-01 21:14:03 +0000

    Starting repository: first commit

---
+++ src/main/java/dev/java/ecommerce/logisticsclean/LogisticsCleanApplication.java
@@ -0,0 +1,13 @@
+package dev.java.ecommerce.logisticsclean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticsCleanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogisticsCleanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/java/ecommerce/logisticsclean/LogisticsCleanApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java.ecommerce.logisticsclean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsCleanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
