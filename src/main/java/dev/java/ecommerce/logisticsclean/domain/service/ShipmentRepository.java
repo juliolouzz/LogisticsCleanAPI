fdf15ca169d184421f0dac5d092b08a57ef609a1
juliolouzz <jlouz.dev@gmail.com>
2025-11-21 23:30:29 +0000

    refactor(domain): change orderId type to String

--- src/main/java/dev/java/ecommerce/logisticsclean/domain/service/ShipmentRepository.java
+++ src/main/java/dev/java/ecommerce/logisticsclean/domain/service/ShipmentRepository.java
@@ -7,6 +7,8 @@ import java.util.Optional;
 // The domain needs some way to save and find shipments.
 public interface ShipmentRepository {
     Shipment save(Shipment shipment);
+
     Optional<Shipment> findById(Long id);
-    Optional<Shipment> findByOrderId(Long orderId);
+
+    Optional<Shipment> findByOrderId(String orderId);
 }
